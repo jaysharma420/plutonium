0ff72396bd8036bf4b81dbf3ce9c7c3184a88aa2
jay123 <jay175147@gmail.com>
2022-08-08 13:45:52 +0530

    abcd

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash = require('lodash');
 const abc = require('../introduction/intro')
 const pqr = require('../logger/logger')
 const stu = require('../util/helper')
@@ -6,16 +7,17 @@ const xyz = require('../validator/formatter')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    pqr.welcome()
-    stu.printDate()
-    stu.printMonth()
-    stu. getBatchInfo()
-    xyz.trim("           function   up       ")
-    xyz.changetoLowerCase("PLS CHANGE THIS INTO A LOWER CASE")
-    xyz.changeToUpperCase("hey bro convert this into upper case")
+    // console.log('My batch is', abc.name)
+    // abc.printName()
+    // pqr.welcome()
+    // stu.printDate()
+    // stu.printMonth()
+    // stu. getBatchInfo()
+    // xyz.trim("           function   up       ")
+    // xyz.changetoLowerCase("PLS CHANGE THIS INTO A LOWER CASE")
+    // xyz.changeToUpperCase("hey bro convert this into upper case")
     res.send('My second ever ever api!')
+    stu.chuk()
 });
 
 
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,3 +1,6 @@
+// const { size } = require('lodash');
+const lodash = require('lodash');
+
 let printDate = function(){
     console.log("today's date is :- ",new Date().toDateString());
 }
@@ -8,6 +11,26 @@ let printMonth = function(){
 let getBatchInfo = function(){
     console.log("Plutonium, W3D3, the topic for today is Nodejs module system");
 }
+
+let chuk = function(){
+    var arr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    var odd = [1,3,5,7,9,11,13,15,17,19]
+var t = lodash.chunk(arr,[size=3])
+let jk = lodash.tail(odd)
+let first = [1,2,3]
+let second = [4,2,3]
+let third = [4,5,3]
+let fourth = [4,5,6]
+let fifth = [6,7,8]
+let sixth = lodash.union(first,second,third,fourth,fifth)
+let st = lodash.fromPairs( [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]])
+
+console.log(st);
+console.log(sixth);
+console.log(jk);
+console.log(t);
+}
 module.exports.printDate = printDate
 module.exports.printMonth = printMonth
 module.exports.getBatchInfo = getBatchInfo
+module.exports.chuk = chuk
