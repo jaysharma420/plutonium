2d361e5f0eb9a6af65efe2e3a3416767b43f5eee
jay123 <jay175147@gmail.com>
2022-08-29 07:27:44 +0530

    abcd

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,98 +1,69 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-/*
-  Read all the comments multiple times to understand why we are doing what we are doing in login api and getUserData api
-*/
-const createUser = async function (abcd, xyz) {
-  //You can name the req, res objects anything.
-  //but the first parameter is always the request 
-  //the second parameter is always the response
-  let data = abcd.body;
-  let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
-};
 
-const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
-  let password = req.body.password;
+const createuser = async function(req,res){
+    let data = req.body
+    let savedata = await userModel.create(data)
+    res.send({status:true,msg:savedata})
+}
 
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
+const login = async function(req,res){
+let username = req.body.emailId
+let password = req.body.password
 
-  // Once the login is successful, create the jwt token with sign function
-  // Sign function has 2 inputs:
-  // Input 1 is the payload or the object containing data to be set in token
-  // The decision about what data to put in token depends on the business requirement
-  // Input 2 is the secret (This is basically a fixed value only set at the server. This value should be hard to guess)
-  // The same secret will be used to decode tokens 
-  let token = jwt.sign(
+let user = await userModel.findOne({emailId:username,password:password})
+if(!user) return res.send({status:false,msg:"username or password is wrong"})
+
+let token = await jwt.sign(
     {
-      userId: user._id.toString(),
-      batch: "thorium",
-      organisation: "FunctionUp",
-    },
-    "functionup-plutonium-very-very-secret-key"
-  );
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, token: token });
-};
+        userid : user._id.toString(),
+        name:"mohan kumar",
+        address:"ayodhya",
+        pin:"102030"
+    },"this is the most important secret key"
+)
+res.setHeader("x-auth-token", token)
+res.send({status:true,msg:token})
+}
 
-const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
 
-  //If no token is present in the request header return error. This means the user is not logged in.
-  if (!token) return res.send({ status: false, msg: "token must be present" });
+const getuser = async function(req,res){
+    let user = req.params.userId
+    if(!user) return res.send({status:false,msg:"user id is not present in path-params"})
+    
+    let udata = await userModel.findById(user)
+    if(!udata) return res.send({status:false,msg:"user id is not valid"})
+res.send({status:true,msg:udata})
+}
 
-  console.log(token);
+const update = async function(req,res){
 
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
+    let user = req.params.userId
 
-  // Decoding requires the secret again. 
-  // A token can only be decoded successfully if the same secret was used to create(sign) that token.
-  // And because this token is only known to the server, it can be assumed that if a token is decoded at server then this token must have been issued by the same server in past.
-  let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
+let data = req.body
+if(!(Object.keys(data).length > 0))return res.send({status:false,msg:"no data present to update"})
 
-  let userId = req.params.userId;
-  let userDetails = await userModel.findById(userId);
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
+let updata = await userModel.findOneAndUpdate(
+    {_id:user},
+    {$set:data},
+    {new:true}
+)
+res.send({status:"updated data",msg:updata})
+}
 
-  res.send({ status: true, data: userDetails });
-  // Note: Try to see what happens if we change the secret while decoding the token
-};
-
-const updateUser = async function (req, res) {
-  // Do the same steps here:
-  // Check if the token is present
-  // Check if the token present is a valid token
-  // Return a different error message in both these cases
-
-  let userId = req.params.userId;
-  let user = await userModel.findById(userId);
-  //Return an error if no user with the given id exists in the db
-  if (!user) {
-    return res.send("No such user exists");
-  }
-
-  let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
-};
-
-module.exports.createUser = createUser;
-module.exports.getUserData = getUserData;
-module.exports.updateUser = updateUser;
-module.exports.loginUser = loginUser;
+const deletedata = async function(req,res){
+    let user = req.params.userId
+   
+    let updata = await userModel.findOneAndUpdate(
+        {_id:user},
+        {$set:{isDeleted:true}},
+        {new:true}
+    )
+    res.send({status:"data deleted",msg:updata})
+}
+module.exports.createuser = createuser
+module.exports.login = login
+module.exports.getuser = getuser
+module.exports.update = update
+module.exports.delete = deletedata
--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://jay420:gRLzeLdOa6ENyasF@cluster0.dnkg3q6.mongodb.net/jay420-DB", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 
 app.use('/', route)
