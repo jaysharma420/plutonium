10dd4853e3b230349b548c9960860e6a7e7f978b
jay123 <jay175147@gmail.com>
2022-08-30 21:10:41 +0530

    abc

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,7 +6,7 @@ const createuser = async function (req, res) {
     try {
     let data = req.body
     let savedata = await userModel.create(data)
-    res.send({ status: true, msg: savedata })
+    res.status(201).send({ status: true, msg: savedata })
 } catch (err) {
     return res.status(500).send({ status: "false", msg: err.message })
 }
@@ -15,11 +15,11 @@ const createuser = async function (req, res) {
 const getuser = async function (req, res) {
 try {
     let user = req.params.userId
-    if (!user) return res.send({ status: false, msg: "user id is not present in path-params" })
+    if (!user) return res.status(400).send({ status: false, msg: "user id is not present in path-params" })
 
     let udata = await userModel.findById(user)
-    if (!udata) return res.send({ status: false, msg: "user id is not valid" })
-    res.send({ status: true, msg: udata })
+    if (!udata) return res.status(404).send({ status: false, msg: "user id is not valid" })
+    res.status(200).send({ status: true, msg: udata })
 } catch (err) {
     return res.status(500).send({ status: "false", msg: err.message })
 }
@@ -29,21 +29,21 @@ const update = async function (req, res) {
 
 try {
     let user = req.params.userId
-    if (!user) return res.send({ status: false, msg: "user id is not present in path-params" })
+    if (!user) return res.status(400).send({ status: false, msg: "user id is not present in path-params" })
 
     let udata = await userModel.findById(user)
-    if (!udata) return res.send({ status: false, msg: "user id is not valid" })
+    if (!udata) return res.status(404).send({ status: false, msg: "user id is not valid" })
 
 
     let data = req.body
-    if (!(Object.keys(data).length > 0)) return res.send({ status: false, msg: "no data present to update" })
+    if (!(Object.keys(data).length > 0)) return res.status(400).send({ status: false, msg: "no data present to update" })
 
     let updata = await userModel.findOneAndUpdate(
         { _id: user },
         { $set: data },
         { new: true }
     )
-    res.send({ status: "updated data", msg: updata })
+    res.status(200).send({ status: "updated data", msg: updata })
 } catch (err) {
     return res.status(500).send({ status: "false", msg: err.message })
 }
@@ -54,17 +54,17 @@ const deletedata = async function (req, res) {
 
     try{
     let user = req.params.userId
-    if (!user) return res.send({ status: false, msg: "user id is not present in path-params" })
+    if (!user) return res.status(400).send({ status: false, msg: "user id is not present in path-params" })
 
     let udata = await userModel.findById(user)
-    if (!udata) return res.send({ status: false, msg: "user id is not valid" })
+    if (!udata) return res.status(404).send({ status: false, msg: "user id is not valid" })
 
     let updata = await userModel.findOneAndUpdate(
         { _id: user },
         { $set: { isDeleted: true } },
         { new: true }
     )
-    res.send({ status: "data deleted", msg: updata })
+    res.status(200).send({ status: "data deleted", msg: updata })
 
 } catch (err) {
     return res.status(500).send({ status: "false", msg: err.message })
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,7 +7,7 @@ const authentication = async function (req, res, next) {
         let password = req.body.password
 
         let user = await userModel.findOne({ emailId: username, password: password })
-        if (!user) return res.send({ status: false, msg: "username or password is wrong" })
+        if (!user) return res.status(404).send({ status: false, msg: "username or password is wrong" })
 
         let token = await jwt.sign(
             {
@@ -17,8 +17,8 @@ const authentication = async function (req, res, next) {
                 pin: "102030"
             }, "this is the most important secret key"
         )
-        res.setHeader("x-auth-token", token)
-        res.send({ status: true, msg: token })
+        res.status(200).setHeader("x-auth-token", token)
+        res.status(200).send({ status: true, msg: token })
 
     } catch (err) {
         return res.status(500).send({ status: "false", msg: err.message })
@@ -28,13 +28,13 @@ const authentication = async function (req, res, next) {
 const authorisation = async function (req, res, next) {
     try {
         let token = req.headers["x-auth-token"]
-        if (!token) return res.send({ status: false, msg: "x-auth-token key is not present in headers" })
+        if (!token) return res.status(400).send({ status: false, msg: "x-auth-token key is not present in headers" })
 
         let decodetoken = await jwt.verify(token, "this is the most important secret key")
         console.log(decodetoken);
-        if (!decodetoken) return res.send({ status: false, msg: "token or signature key is missing" })
+        if (!decodetoken) return res.status(401).send({ status: false, msg: "token or signature key is missing" })
 
-        if (!(decodetoken.userid === req.params.userId)) return res.send({ status: false, msg: "token is not verified" })
+        if (!(decodetoken.userid === req.params.userId)) return res.status(403).send({ status: false, msg: "token is not verified" })
         next()
     } catch (err) {
         return res.status(500).send({ status: "false", msg: err.message })
