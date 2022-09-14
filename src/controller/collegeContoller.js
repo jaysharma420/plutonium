748bab1e8d8ae06f6ae55ba6e388913d21ac9246
Richard-vinu <richardnova1999@gmail.com>
2022-09-14 09:47:50 +0530

    formate

--- src/controller/collegeContoller.js
+++ src/controller/collegeContoller.js
@@ -1,35 +1,18 @@
-
-const collegeModel = require('../models/CollegeModel')
-const internsModel = require('../models/internsModel')
-
-
+const collegeModel = require("../models/CollegeModel");
+const internsModel = require("../models/internsModel");
 
 //validation by => JAY Sharama
-const createCollege = async (req,res)=>{
+const createCollege = async (req, res) => {
+  try {
+    let data = req.body;
 
-    try{
-        let data = req.body
+    const datas = await college.create(data);
 
-
-        const datas =await college.create(data)
-    
-        res.status(201).send({status:true,data:datas})
-
-    }catch(err){
-        res.status(500).send({status:false,message:err.message})
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
+    res.status(201).send({ status: true, data: datas });
+  } catch (err) {
+    res.status(500).send({ status: false, message: err.message });
+  }
+};
 
 //------------------⭐GET/collegeDetails⭐---------------------------//
 
@@ -44,64 +27,55 @@ const createCollege = async (req,res)=>{
 //     res.send({data:datas})
 // }
 
-const getCollegeDetails = async(req,res)=>{
+const getCollegeDetails = async (req, res) => {
+  try {
+    const clgName = req.query;
+    let { collegeName } = req.query;
 
-try{
+    if (!collegeName)
+      return res
+        .status(400)
+        .send({ status: false, msg: "CollgeName is required" });
 
-      const clgName = req.query
-     let {collegeName} = req.query
+    if (Object.keys(clgName).length > 1)
+      return res.status(400).send({ status: false, msg: "enter single query" });
 
-     if(!collegeName)return res.status(400).send({status:false,msg:"CollgeName is required"})
+    const isValidName = function (value) {
+      if (!(value === value.toLowerCase())) {
+        return false;
+      }
+      return true;
+    };
 
-     if((Object.keys(clgName).length>1))return res.status(400).send({status:false,msg:"enter single query"})
+    if (!isValidName(collegeName))
+      return res .status(400).send({status: false,msg: "Invalid-CollegeName-Try name with lowerCase",});
 
-     const isValidName = function (value) {
-        if (!(value === value.toLowerCase())) {
-            return false }
-        return true
-    }
+     const collegename = await collegeModel.findOne({ name: collegeName });
 
-    if(!isValidName(collegeName))return res.status(400).send({status:false,msg:"Invalid-CollegeName-Try name with lowerCase"})
+    if (!collegename)return res.status(404)
+        .send({ status: false, msg: "This College not Found" });
 
-    const  collegename = await collegeModel.findOne({name:collegeName})
+    const { name, fullName, logoLink } = collegename;
 
-    if(!collegename)return res.status(404).send({status:false,msg:"This College not Found"})
+    const intern = await internsModel
+      .find({ collegeId: collegename._id })
+      .select({ name: 1, email: 1, mobile: 1 });
 
-    const{name,fullName,logoLink} = collegename
+    const data = {
+      name: name,
+      fullName: fullName,
+      logoLink: logoLink,
+      interns: intern.length
+        ? intern
+        : { msg: "0 application from this collge" },
+    };
 
-    
-
-    const intern = await internsModel.find({collegeId:collegename._id}).select({name:1,email:1,mobile:1})
-
-
-   const data = {
-    name:name,
-    fullName:fullName,
-   logoLink:logoLink,
-   interns:intern.length ? intern:{msg:"0 application from this collge"}
-   
-   }
-
-
-  return res.status(200).send({status:true,data:data})
-
-
-
-}catch(err){return res.status(500).send({status:false,msg:err.message})}}
-    
-
-
-
-    
-
-
-
-   
-
-
-
+    return res.status(200).send({ status: true, data: data });
+  } catch (err) {
+    return res.status(500).send({ status: false, msg: err.message });
+  }
+};
 
 // module.exports = {createIntern}
 
-
-module.exports = {createCollege,getCollegeDetails}
+module.exports = { createCollege, getCollegeDetails };
